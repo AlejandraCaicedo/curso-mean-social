ef8649ff113072180ff2f267253c278dd802fe58
AlejandraCaicedo <caicedomaria905@gmail.com>
2024-07-05 15:38:33 -0500

    New method save publication

--- src/controllers/publication.js
+++ src/controllers/publication.js
@@ -8,6 +8,7 @@ var mongoosePaginate = require('mongoose-pagination');
 var Publication = require('../models/publication');
 var User = require('../models/user');
 var Follow = require('../models/follow');
+const { publicDecrypt } = require('crypto');
 
 function prrueba(req, res) {
 	res.status(200).send({
@@ -15,6 +16,37 @@ function prrueba(req, res) {
 	});
 }
 
+function savePublication(req, res) {
+	let params = req.body;
+
+	if (!params.text) {
+		return res.status(200).send({ message: 'You must send a text' });
+	}
+
+	let publication = new Publication();
+	publication.text = params.text;
+	publication.file = null;
+	publication.user = req.user.sub;
+	publication.created_at = moment().unix();
+
+	publication.save((err, publicationStored) => {
+		if (err)
+			return res.status(500).send({
+				message: 'Error saving the publication.',
+			});
+
+		if (!publicationStored)
+			return res.status(404).send({
+				message: 'The publication has not been saved.',
+			});
+
+		return res.status(200).send({
+			publication: publicationStored,
+		});
+	});
+}
+
 module.exports = {
 	prrueba,
+	savePublication,
 };
--- src/routers/publication.js
+++ src/routers/publication.js
@@ -7,3 +7,12 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 var multipart = require('connect-multiparty');
+var md_upload = multipart({ uploadDit: './uploads/publications' });
+
+api.post(
+	'/publication',
+	md_auth.ensureAuth,
+	PublicationController.savePublication,
+);
+
+module.exports = api;
